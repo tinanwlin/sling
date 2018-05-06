09076890daf5fe69143300ebf4ce7d60e188f970
tinanwlin <tinanwlin@gmail.com>
2018-05-06 02:23:30 -0700

    phoenix setup

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -37,8 +37,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :sling, Sling.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "sling_dev",
   hostname: "localhost",
   pool_size: 10
+
+
