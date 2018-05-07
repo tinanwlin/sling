fd85efa5782dc6dad0e43d6e7d8ef667c174c996
tinanwlin <tinanwlin@gmail.com>
2018-05-07 08:35:00 -0700

    create user model

--- api/mix.exs
+++ api/mix.exs
@@ -20,7 +20,8 @@ defmodule Sling.Mixfile do
   def application do
     [
       mod: {Sling, []},
-      extra_applications: [:logger]
+      extra_applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
+                  :phoenix_ecto, :postgrex, :comeonin]
     ]
   end
 
@@ -37,8 +38,11 @@ defmodule Sling.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:comeonin, "~> 2.5"}
     ]
   end
 
